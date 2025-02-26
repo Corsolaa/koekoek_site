6c3baa94cc408bf711ad181850afe3833d2c95b4
BrunoB <bruno@corsolaa.nl>
2025-02-26 19:55:03 +0100

    init

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="author" content="John Spice">
+    <meta name="description" content="a very birdy website">
+    <title>Koekoek BV</title>
+
+    <link rel="stylesheet" href="assets/standards.css">
+    <link rel="stylesheet" href="assets/style.css">
+
+    <link rel="icon" href="icon.ico">
+
+    <!-- google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <div class="logo_container">
+        <img src="icon.ico" alt="website icon">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#e6b400" d="M40.3,-52.4C52.9,-46.3,64.2,-35.4,69.7,-21.9C75.1,-8.3,74.7,7.9,68.1,20.3C61.6,32.7,49,41.4,36.6,47.5C24.2,53.7,12.1,57.4,-3.3,61.9C-18.7,66.5,-37.5,71.9,-46.9,64.8C-56.4,57.7,-56.6,38.1,-58.4,21.6C-60.3,5,-63.7,-8.4,-62.4,-22.6C-61,-36.9,-54.9,-51.9,-43.7,-58.5C-32.5,-65.1,-16.2,-63.2,-1.2,-61.5C13.8,-59.9,27.7,-58.5,40.3,-52.4Z" transform="translate(100 100)" />
+        </svg>
+
+    </div>
+
+    <h1>Koekoek BV</h1>
+</header>
+
+<main>
+    <section class="content">
+        <a class="koekoek_link" target="_blank" href="https://youtu.be/982YE6IrH6A?t=501">koekoek jonge!</a>
+
+        <br>
+
+        click me ...
+    </section>
+</main>
+
+<footer>
+    <p style="color: white">made with ❤️ by Bruno</p>
+</footer>
+
+</body>
+</html>
