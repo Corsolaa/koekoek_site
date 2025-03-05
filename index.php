6bc2b3bb6ef2cedcf7a855bfb41ec97cefb12ae6
Bruno B 🦆 <bruno@oversticht.eu>
2025-03-05 23:30:31 +0100

    yes, header and mobile navigation

--- index.php
+++ index.php
@@ -22,15 +22,23 @@
 <body>
 
 <header>
-    <div class="logo_container">
-        <img src="icon.ico" alt="website icon">
-        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-            <path fill="#e6b400" d="M40.3,-52.4C52.9,-46.3,64.2,-35.4,69.7,-21.9C75.1,-8.3,74.7,7.9,68.1,20.3C61.6,32.7,49,41.4,36.6,47.5C24.2,53.7,12.1,57.4,-3.3,61.9C-18.7,66.5,-37.5,71.9,-46.9,64.8C-56.4,57.7,-56.6,38.1,-58.4,21.6C-60.3,5,-63.7,-8.4,-62.4,-22.6C-61,-36.9,-54.9,-51.9,-43.7,-58.5C-32.5,-65.1,-16.2,-63.2,-1.2,-61.5C13.8,-59.9,27.7,-58.5,40.3,-52.4Z" transform="translate(100 100)" />
-        </svg>
-
+    <div class="logo flex_center">
+        <img src="images/koekoek_logo.webp" alt="">
     </div>
 
-    <h1>Koekoek BV</h1>
+    <nav class="flex_center">
+        <ul class="only_show_mobile">
+            mobile nav
+        </ul>
+        <ul class="hide_mobile">
+            <li><a href="">Project</a></li>
+            <li><a class="selected" href="">Consult</a></li>
+            <li><a href="">Beheer</a></li>
+        </ul>
+        <div class="quick flex_center">
+            <button>Contact</button>
+        </div>
+    </nav>
 </header>
 
 <main>
