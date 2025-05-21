34030a4533ef2f6ab864aad450660bac05002750
Bruno B 🦆 <bruno@oversticht.eu>
2025-05-21 08:56:41 +0200

    new svg burger logo

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,14 @@
 const mobile_nav = document.querySelector('nav.mobile');
 const mobile_nav_button = document.querySelector('.mobile_nav_button');
 const body = document.querySelector('body');
+const mobile_nav_close_button = document.querySelector('.mobile_nav_close_button');
+const mobile_nav_open_button = document.querySelector('.mobile_nav_open_button');
 
 mobile_nav_button.addEventListener('click', () => {
     mobile_nav.classList.toggle('active');
     body.classList.toggle('overflow-hidden');
+    mobile_nav_close_button.classList.toggle('hidden');
+    mobile_nav_open_button.classList.toggle('hidden');
 });
 
 const mobile_nav_items = mobile_nav.querySelectorAll('.item');
